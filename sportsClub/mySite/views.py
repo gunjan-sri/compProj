0069e315abb077bbc482844667937f544be1f4c6
amisharajput <68576310+amisharajput@users.noreply.github.com>
2020-09-24 15:21:51 +0900

    done

--- sportsClub/mySite/views.py
+++ sportsClub/mySite/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+import csv
 # Create your views here.
+def basketball(reuqest):
+    if request.method=="POST":
+        dict1=request.POST
+        with open("basketball.csv","a") as csvfile:
+            wrt=csv.writer(csvfile)
+            for key, value in dict1.items():
+                wrt.writerow([key,value])
+
+    return render(reuqest,"baskteball.html")
+
+def badminton(request):
+    if request.method=="POST":
+        dict1=request.POST
+        with open("badminton.csv","a") as csvfile:
+            wrt=csv.writer(csvfile)
+            for key, value in dict1.items():
+                wrt.writerow([key,value])
+
+    return render(reuqest,"badminton.html")
+
+def dance(request):
+    if request.method=="POST":
+        dict1=request.POST
+        with open("dance.csv","a") as csvfile:
+            wrt=csv.writer(csvfile)
+            for key, value in dict1.items():
+                wrt.writerow([key,value])
+
+    return render(reuqest,"baskteball.html")
+
+
+
