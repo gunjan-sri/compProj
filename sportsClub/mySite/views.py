5e4efae3abbef084e1e71d72287c27d9fd40589c
amisharajput <68576310+amisharajput@users.noreply.github.com>
2020-09-24 11:44:49 +0900

    Update views.py

--- sportsClub/mySite/views.py
+++ sportsClub/mySite/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+import csv
+
 
 # Create your views here.
+def basketball(request):
+    return render(request, "basketball.html")
+
+def badminton(request):
+    return render(request, "badminton.html")
+
+def dance(request):
+    return render(request, "dance.html")
+
+def faq(request):
+    return render(request, "faq.html")
+
+def fees(request):
+    return render(request, "fees.html")
+
+def food(request):
+    return render(request, "food.html")
+
+def gym(request):
+    return render(request, "gym.html")
+
+def gymnastics(request):
+    return render(request, "gymnastics.html")
+
+def homepage(request):
+    return render(request, "homepage.html")
+
+def lawntennis(request):
+    return render(request, "lawntennis.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+def squash(request):
+    return render(request, "squash.html")
+
+def swimming(request):
+    return render(request, "swimming.html")
+
+def tabletennis(request):
+    return render(request, "tabletennis.html")
+
+
+
+
+
+   
+
+   
