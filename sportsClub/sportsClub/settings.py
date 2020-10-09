c8030b4c78f1eb9f77f70437615c2d59117200b0
amisharajput <68576310+amisharajput@users.noreply.github.com>
2020-10-09 15:19:33 +0900

    v

--- sportsClub/sportsClub/settings.py
+++ sportsClub/sportsClub/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,4 +126,5 @@ STATIC_URL = '/templates/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, "templates")
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'templates')
+
+
