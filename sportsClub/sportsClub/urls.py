f4370dd3cfd24ceac1865aa041ba1da2f3944f9d
amisharajput <68576310+amisharajput@users.noreply.github.com>
2020-10-09 11:23:06 +0900

    added django to homepage

--- sportsClub/sportsClub/urls.py
+++ sportsClub/sportsClub/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from mySite import views as siteviews
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/',siteviews.homepage),
